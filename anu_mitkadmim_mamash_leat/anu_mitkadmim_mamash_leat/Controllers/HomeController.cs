6960f50ae64dbfc5189687e2e2d1b98379423b3e
AviBraun123 <avraham.brauner@live.biu.ac.il>
2022-05-10 11:26:56 +0300

    try

---
+++ anu_mitkadmim_mamash_leat/anu_mitkadmim_mamash_leat/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using anu_mitkadmim_mamash_leat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace anu_mitkadmim_mamash_leat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ anu_mitkadmim_mamash_leat/anu_mitkadmim_mamash_leat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace anu_mitkadmim_mamash_leat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
