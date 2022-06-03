fb78828fe608fd823ab3b62ba5fc83a9bd1d869a
MatanShn <102690877+MatanShn@users.noreply.github.com>
2022-06-03 15:00:02 +0300

    Add files via upload

---
+++ anu_mitkadmim_mamash_leat/anu_mitkadmim_mamash_leat/Controllers/UsersController.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using anu_mitkadmim_mamash_leat;
+using anu_mitkadmim_mamash_leat.Data;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace anu_mitkadmim_mamash_leat.Controllers
+{
+    [ApiController]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        public IConfiguration _configuration;
+
+        private readonly anu_mitkadmim_mamash_leatContext _context;
+
+        public UsersController(IConfiguration configuration, anu_mitkadmim_mamash_leatContext context)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Post(string username)
+        {
+            
+                var claims = new[]
+                {
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, _configuration["JWTParams:Subject"]),
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("userId", username)
+                };
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTParams:SecretKey"]));
+                var mac = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(_configuration["JWTParams:Issuer"], _configuration["JWTParams:Audience"], claims, expires: DateTime.UtcNow.AddMinutes(60), signingCredentials: mac);
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            
+        }
+
+        [HttpPost("reg")]
+        public async Task<IActionResult> reg(string id, string nickname, string password, string? profpic)
+        {
+            //var q = from r in _context.User
+            //        where r.id == id
+            //        select r;
+            User u = new User();
+            u.id = id; 
+            u.profpic = profpic;
+            u.nickname = nickname; 
+            u.password = password;
+            _context.User.Add(u);
+            await _context.SaveChangesAsync();
+            var claims = new[]
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, _configuration["JWTParams:Subject"]),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("userId", id)
+            };
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTParams:SecretKey"]));
+            var mac = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_configuration["JWTParams:Issuer"], _configuration["JWTParams:Audience"], claims, expires: DateTime.UtcNow.AddMinutes(60), signingCredentials: mac);
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+
+        //GET: Users
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Json(await _context.User.ToListAsync());
+        }
+
+        //GET: Users/Details/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Json(user);
+        }
+
+        //// GET: Users/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Users/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("id,nickname,password,profpic")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.User.Add(user);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return BadRequest();
+        //}
+
+        // GET: Users/Edit/5
+        
+        //public async Task<IActionResult> Edit(string id)
+        //{
+        //    if (id == null || _context.User == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var user = await _context.User.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(user);
+        //}
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPut("{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("id,nickname,password,profpic")] User user)
+        {
+            if (id != user.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        //// GET: Users/Delete/5
+        //public async Task<IActionResult> Delete(string id)
+        //{
+        //    if (id == null || _context.User == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var user = await _context.User
+        //        .FirstOrDefaultAsync(m => m.id == id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(user);
+        //}
+
+        // POST: Users/Delete/5
+        [HttpDelete("{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.User == null)
+            {
+                return Problem("Entity set 'anu_mitkadmim_mamash_leatContext.User'  is null.");
+            }
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                _context.User.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(string id)
+        {
+            return (_context.User?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
