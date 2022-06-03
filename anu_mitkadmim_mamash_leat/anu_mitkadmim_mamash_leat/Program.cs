ee4db03a25297560e5f28a538a23d1010f3f813b
AviBraun123 <102579667+AviBraun123@users.noreply.github.com>
2022-06-03 15:20:57 +0300

    Add files via upload

--- anu_mitkadmim_mamash_leat/anu_mitkadmim_mamash_leat/Program.cs
+++ anu_mitkadmim_mamash_leat/anu_mitkadmim_mamash_leat/Program.cs
@@ -1,8 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using anu_mitkadmim_mamash_leat.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using anu_mitkadmim_mamash_leat.Controllers;
+using Microsoft.AspNetCore.Builder;
+using anu_mitkadmim_mamash_leat.hub;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+builder.Services.AddDbContext<anu_mitkadmim_mamash_leatContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("anu_mitkadmim_mamash_leatContext") ?? throw new InvalidOperationException("Connection string 'anu_mitkadmim_mamash_leatContext' not found.")));
+
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWTParams:Audience"],
+        ValidIssuer = builder.Configuration["JWTParams:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTParams:SecretKey"]))
+    };
+});
+
+builder.Services.AddScoped<serviceContacts>();
+builder.Services.AddScoped<indipandesService>();
+
+/*builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow All", builder => { builder.WithOrigins("http://localhost:3000/").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+});
+*/
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow All", builder => { builder.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials(); });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -10,14 +52,34 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCors("Allow All");
+
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<Class>("/Class");
+});
 
 app.Run();
